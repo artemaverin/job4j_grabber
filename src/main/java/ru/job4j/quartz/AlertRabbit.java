8b076fe1e70d87491a19154315e0ccba4012397d
Artem Averin <artem-averin-91@mail.ru>
2023-03-09 00:09:07 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,9 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -13,10 +16,14 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
     public static void main(String[] args) {
-        try {
+        try (Connection connection = init()) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("conection", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             int param = Integer.parseInt(propReader().getProperty("rabbit.interval"));
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(param)
@@ -26,9 +33,21 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+            System.out.println(connection);
+        } catch (Exception se) {
             se.printStackTrace();
         }
+    }
+
+    public static Connection init() throws ClassNotFoundException, SQLException {
+        Class.forName(propReader().getProperty("driver-class-name"));
+        String url = propReader().getProperty("url");
+        String login = propReader().getProperty("username");
+        String password = propReader().getProperty("password");
+        Connection connect = DriverManager.getConnection(url, login, password);
+        return connect;
     }
 
     public static Properties propReader() {
@@ -43,9 +62,24 @@ public class AlertRabbit {
     }
 
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
+
             System.out.println("Rabbit runs here ...");
+            Connection conection = (Connection) context.getJobDetail().getJobDataMap().get("conection");
+            try (PreparedStatement preparedStatement = init().prepareStatement(
+                    "insert into rabbit(created_date) values (?);",
+                    Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+                preparedStatement.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
