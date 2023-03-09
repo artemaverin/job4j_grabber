20edcb56956a57bc7a1698f485e979c529e8c8a2
Artem Averin <artem-averin-91@mail.ru>
2023-03-09 18:44:07 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -8,6 +8,7 @@ import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
+import javax.print.Doc;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -15,12 +16,12 @@ import java.util.Date;
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
 
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
         for (int i = 1; i < 6; i++) {
             System.out.println("Page # " + i);
-            Connection connection = Jsoup.connect(PAGE_LINK + "?page=" + i);
+            Connection connection = Jsoup.connect(PAGE_LINK + i);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
@@ -32,9 +33,28 @@ public class HabrCareerParse {
                 String date = dateElement.attr("datetime");
                 DateTimeParser hcdtp = new HabrCareerDateTimeParser();
                 LocalDateTime dateFormat = hcdtp.parse(date);
-                System.out.printf("%s %s %s%n", vacancyName, link, dateFormat);
+                String description = new HabrCareerParse().retrieveDescription(link);
+                System.out.printf("%s %s %s %s%n", vacancyName, link, dateFormat, description);
             });
         }
 
+    }
+
+    private String retrieveDescription(String link) {
+        Connection connection = Jsoup.connect(link);
+        Document document = null;
+        try {
+            document = connection.get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements rows = document.select(".vacancy-description__text");
+        rows.forEach(
+                row -> {
+                    Element element = row.child(0);
+                }
+        );
+        String description = rows.text();
+        return description;
     }
 }
